b4f5934ed22d665cb439b941dc6f8da2a4e44ebd
ibnu030303 <ibunu.nurdiyansa28@gmail.com>
2024-06-27 00:00:33 +0700

    Updated Fixing Build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                "vendor/filament/filament/resources/css/theme.css",
+                'resources/css/app.css',
+                'resources/js/app.js',
+                // Pastikan jalur ini sesuai dengan struktur direktori Anda
+                'vendor/filament/filament/resources/css/theme.css'
             ],
             refresh: true,
         }),
